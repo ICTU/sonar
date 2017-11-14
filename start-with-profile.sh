6e712a78d943858d54a7a7efab7781d62f561791
Eduardo Lucas <eduardo.lucas@ictu.nl>
2017-11-14 09:53:01 +0100

    Fix installation of vb plugin

--- start-with-profile.sh
+++ start-with-profile.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Start Sonar
-./bin/run.sh &
-
-BASE_URL=http://127.0.0.1:9000
-
 # Access SonarQube api with admin credentials
 function curlAdmin {
     curl -v -u admin:admin $@
@@ -13,6 +8,28 @@ function curlAdmin {
 # Check is SonarQube is running
 function isUp {
     curl -s -u admin:admin -f "$BASE_URL/api/system/info"
+}
+
+# Wait until SonarQube is operational
+function waitForSonarUp {
+    # Wait for server to be up
+    while [ -z "$PING" ]
+    do
+        echo "Waiting for sonar to come up"
+        sleep 5
+        PING=`isUp`
+    done
+}
+
+# Wait until SonarQube is operational
+function waitForSonarDown {
+    # Wait for server to be up
+    while [ ! -z "$PING" ]
+    do
+        echo "Waiting for sonar to come down"
+        sleep 5
+        PING=`isUp`
+    done
 }
 
 # Create a new SonarQube profile with custom activated rules, inheritance and set as default
@@ -50,14 +67,16 @@ function createProfile {
     curlAdmin -X POST "$BASE_URL/api/qualityprofiles/set_default?profileName=$1&language=$3"
 }
 
-# Wait for server to be up
-PING=`isUp`
-while [ -z "$PING" ]
-do
-    sleep 5
-    PING=`isUp`
-done
+###########################################################################################################################
+# Main
+###########################################################################################################################
+BASE_URL=http://127.0.0.1:9000
 
+# Start Sonar
+./bin/run.sh &
+waitForSonarUp
+
+# (Re-)create the ICTU profiles
 createProfile "ictu-cs-profile-6.5" "Sonar%20way" "cs" "common-cs:DuplicatedBlocks,csharpsquid:S104,csharpsquid:S134,csharpsquid:S1067,csharpsquid:S1541"
 createProfile "ictu-java-profile-4.14" "Sonar%20way" "java" "squid:MethodCyclomaticComplexity,squid:NoSonar,squid:S1067,squid:S109"
 createProfile "ictu-py-profile-6.5" "Sonar%20way" "py" "common-py:DuplicatedBlocks,python:S104,python:S134"
@@ -69,13 +88,10 @@ createProfile "ictu-web-profile-2.5" "Sonar%20way" "web" "common-web:DuplicatedB
 # Adding it to plugin-list is not working (causint SQ initilization error "There is already a quality profile with name 'Sonar way' for language 'vb'")
 curlAdmin -X POST "$BASE_URL/api/plugins/install?key=vbnet"
 curlAdmin -X POST "$BASE_URL/api/system/restart"
+echo "Restarting Sonarqube after installing vb plugin"
+waitForSonarDown
+waitForSonarUp
 
-# Wait for server to be up
-while [ -z "$PING" ]
-do
-    sleep 5
-    PING=`isUp`
-done
-createProfile "ictu-vb-profile-4.0" "Sonar%20way" "vbnet" "common-web:DuplicatedBlocks,Web:ComplexityCheck,Web:LongJavaScriptCheck,Web:S1443"
+createProfile "ictu-vb-profile-4.0" "Sonar%20way" "vbnet" "common-vbnet:DuplicatedBlocks,vbnet:S104,vbnet:S1067,vbnet:S134,vbnet:S1541"
 
 wait
