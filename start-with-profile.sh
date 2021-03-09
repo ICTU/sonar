7f46727d2c0cd9e920d2317bd3c245e1f131f2be
Ramses van Pinxteren <ramses.vanpinxteren@ictu.nl>
2021-03-09 13:32:42 +0100

    ITB-18621: Sonar 8.7

--- start-with-profile.sh
+++ start-with-profile.sh
@@ -240,13 +240,13 @@ testAdminCredentials
 
 # (Re-)create the ICTU profiles
 createProfile "ictu-ts-profile-v2.1.0" "Sonar%20way%20recommended" "ts"
-createProfile "ictu-vbnet-profile-v8.15.0" "Sonar%20way" "vbnet"
-createProfile "ictu-cs-profile-v8.15.0" "Sonar%20way" "cs"
+createProfile "ictu-vbnet-profile-v8.18.0" "Sonar%20way" "vbnet"
+createProfile "ictu-cs-profile-v8.40.0" "Sonar%20way" "cs"
 createProfile "ictu-ansible-profile-v2.4.0" "Sonar%20way" "yaml"
-createProfile "ictu-java-profile-v6.9.0" "Sonar%20way" "java"
-createProfile "ictu-js-profile-v7.0.1" "Sonar%20way%20Recommended" "js"
-createProfile "ictu-kotlin-profile-v1.8.1" "Sonar%20way" "kotlin"
-createProfile "ictu-py-profile-v3.1.0" "Sonar%20way" "py"
+createProfile "ictu-java-profile-v6.12.0" "Sonar%20way" "java"
+createProfile "ictu-js-profile-v7.2.0" "Sonar%20way%20Recommended" "js"
+createProfile "ictu-kotlin-profile-v1.8.2" "Sonar%20way" "kotlin"
+createProfile "ictu-py-profile-v3.2.0" "Sonar%20way" "py"
 createProfile "ictu-web-profile-v3.3.0" "Sonar%20way" "web"
 
 wait $PID
