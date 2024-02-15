000e81dcba068b0f62fce432fe0dd79e1b5deb96
wkoot <3715211+wkoot@users.noreply.github.com>
2024-02-15 21:18:28 +0100

    test env var in test

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,7 +1,10 @@
 from os import getenv
-from unittest import TestCase
+from unittest import TestCase, skipUnless
 
 from sonarqube import SonarQubeClient
+
+PROJECT_CODE = getenv("PROJECT_CODE")
+PROJECT_RULES = getenv("PROJECT_RULES")
 
 
 class SonarTest(TestCase):
@@ -15,3 +18,21 @@ class SonarTest(TestCase):
         java_quality_profiles = self.sonar_client.qualityprofiles.search_quality_profiles(language="java")
         java_profile_names = [profile["name"] for profile in java_quality_profiles["profiles"]]
         self.assertIn("Sonar way", java_profile_names)
+
+    @skipUnless(PROJECT_CODE, "PROJECT_CODE was not passed")
+    def test_csharpsquid_profile(self):
+        search_result = self.sonar_client.qualityprofiles.search_quality_profiles(
+            defaults="true", language="cs", qualityProfile=f"{PROJECT_CODE}-ictu-cs-profile-v9.13.0-20231222"
+        )
+        self.assertEqual(len(search_result['profiles']), 1)
+        cs_profile_key = search_result['profiles'][0]['key']
+        self.assertIsNotNone(cs_profile_key)  # TODO - check activated rules within profile instead
+
+    @skipUnless(PROJECT_CODE, "PROJECT_CODE was not passed")
+    def test_ts_profile(self):
+        search_result = self.sonar_client.qualityprofiles.search_quality_profiles(
+            defaults="true", language="ts", qualityProfile=f"{PROJECT_CODE}-ictu-ts-profile-v10.9.0-20231222"
+        )
+        self.assertEqual(len(search_result['profiles']), 1)
+        ts_profile_key = search_result['profiles'][0]['key']
+        self.assertIsNotNone(ts_profile_key)  # TODO - check activated rules within profile instead
